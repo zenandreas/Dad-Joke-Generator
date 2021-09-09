1234fd36cd8bda2ad22313a5affd74d2d8c02f3d
Andre Romero <romeroandre801@outlook.com>
2021-09-09 15:09:50 -0600

    added html tag to add javascript to index.html, created test code to see if javascript linked to html

---
+++ Resources/Javascrpt/script.js
@@ -0,0 +1,8 @@
+/* On Click of button element should run this code*/
+
+// test to determine html submit button is functioning
+function testFunction() {
+    document.getElementById("Submit").innerHTML = "your javascript is working in HTML";
+  };
+
+
